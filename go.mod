e15c5857da1d1acd9b905f1c42b75701e205c4ca
porag_faisal123@hotmail.com <porag_faisal123@hotmail.com>
2022-07-13 21:51:49 +0600

    sample handler added

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/web-app-with-golang
 
 go 1.18
+
+require github.com/joho/godotenv v1.4.0 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"github.com/joho/godotenv"
+	"log"
+	"net/http"
+	"os"
+)
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	_, err := w.Write([]byte("<h1>Hello World!</h1>"))
+	if err != nil {
+		return
+	}
+}
 
 func main() {
-	fmt.Println("Welcome to web app")
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Error loading .env file")
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", indexHandler)
+	errListener := http.ListenAndServe(":"+port, mux)
+	if errListener != nil {
+		return
+	}
 }

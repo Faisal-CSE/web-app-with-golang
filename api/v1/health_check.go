21434ded300607335b802a9216bd59ecf5888226
porag_faisal123@hotmail.com <porag_faisal123@hotmail.com>
2022-07-15 20:24:04 +0600

    health check api added

---
+++ api/v1/health_check.go
@@ -0,0 +1,26 @@
+package v1
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type Article struct {
+	Id      string `json:"id"`
+	Title   string `json:"title"`
+	Desc    string `json:"desc"`
+	Content string `json:"content"`
+}
+
+func SampleHealthCheckAPI(w http.ResponseWriter, _ *http.Request) {
+	fmt.Println("Rest API v1.0 - Health Check")
+	Articles := []Article{
+		{Id: "000000001", Title: "Sample Article 1", Desc: "Article Description 1", Content: "Article Content 1"},
+		{Id: "000000002", Title: "Sample Article 2", Desc: "Article Description 2", Content: "Article Content 2"},
+	}
+	err := json.NewEncoder(w).Encode(Articles)
+	if err != nil {
+		return
+	}
+}
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
+	v1API "github.com/web-app-with-golang/api/v1"
 	"github.com/web-app-with-golang/controller"
 	"log"
 	"net/http"
@@ -10,6 +11,7 @@ import (
 )
 
 func main() {
+	var strAPIVersion1 string
 	err := godotenv.Load()
 	if err != nil {
 		log.Println("Error loading .env file")
@@ -23,9 +25,13 @@ func main() {
 	route := mux.NewRouter()
 
 	route.HandleFunc("/", controller.IndexController).Methods("GET")
-	route.HandleFunc("/login", controller.LoginController).Methods("GET")
+	route.HandleFunc("/login", controller.LoginController)
 	route.HandleFunc("/contact", controller.ContactController)
 	route.HandleFunc("/test", controller.DummyController)
+
+	// API URL VERSION 1
+	strAPIVersion1 = "/api/v1/"
+	route.HandleFunc(strAPIVersion1+"sample-health-check", v1API.SampleHealthCheckAPI).Methods("GET")
 
 	errListener := http.ListenAndServe(":"+port, route)
 	if errListener != nil {

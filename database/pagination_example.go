17f510c06342d3de651e1369bf3983cee8b29afe
porag_faisal123@hotmail.com <porag_faisal123@hotmail.com>
2022-07-22 20:02:35 +0600

    get record by ID function added

--- database/pagination_example.go
+++ database/pagination_example.go
@@ -5,6 +5,29 @@ import (
 	"log"
 )
 
+func GetRecordByID(Id int) {
+	SQL := `SELECT "id","name" FROM "clientes" WHERE id = $1`
+
+	rows, err := db.Queryx(SQL, Id)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	for rows.Next() {
+		p := struct {
+			ID   int    `db:"id"`
+			Name string `db:"name"`
+		}{}
+		err = rows.StructScan(&p)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		fmt.Printf("ID: %v\tClient Name: %v\n", p.ID, p.Name)
+	}
+}
+
 func ListRecords(page int) {
 
 	limit := 5
